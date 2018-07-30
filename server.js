d164d667aaa024cc83db18f5e5b0094c7ee88906
Tom North <tnorth93@gmail.com>
2018-07-30 12:25:11 -0700

    server has been setup

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Dependencies
+const express = require('express');
+const pg = require('pg');
+const cors = require('cors');
+
+// Setup
+// Tom: This conString variable is setup for use with Windows systems
+const conString = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/trial_by_trivia';
+const app = express();
+const PORT = process.env.PORT || 3000;
+const client = new pg.Client(conString);
+
+client.connect();
+client.on('error', err => console.error(err));
+
+// middleware
+app.use(cors());
+
+
+
+
+app.listen(PORT, () => console.log(`Port ${PORT} engaged`));
